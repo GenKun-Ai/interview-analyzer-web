64331326edc09dfb5e87628ee8b345704afd1f31
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2026-01-11 19:25:42 +0900

    refator: AuthProvider 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ProtectedRoute } from "./components/common/ProtectedRoute";
 import { Layout } from "./components/layout/Layout";
 import { Home } from "./pages/Home/Home";
 import { SessionDetail } from "./pages/SessionDetail/SessionDetail";
+import { Login } from "./pages/Login/Login";
 import "./App.css";
 
 function App() {
-
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Layout을 감싸는 라우트 */}
-        <Route path="/" element={<Layout />}>
-          {/* 홈 페이지 */}
-          <Route index element={<Home />} />
+      <AuthProvider>
+        <Routes>
+          {/* 로그인 페이지 */}
+          <Route path="/login" element={<Login />} />
 
-          {/* 세션 상세 페이지 */}
-          <Route path="session/:id" element={<SessionDetail />} /> 
+          {/* Layout을 감싸는 라우트 (인증 필요) */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Layout />
+              </ProtectedRoute>
+            }
+          >
+            {/* 홈 페이지 */}
+            <Route index element={<Home />} />
 
-          {/* 404 - 존재하지 않는 경로 */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+            {/* 세션 상세 페이지 */}
+            <Route path="session/:id" element={<SessionDetail />} />
+
+            {/* 404 - 존재하지 않는 경로 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
